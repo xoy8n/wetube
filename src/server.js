dc465a1dde371bd156e5809781c08fcf90d3dbc4
xoy8n <s01057111917@gmail.com>
2021-12-23 10:01:39 +0900

    #3.1 Middelware using Morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
+import morgan from "morgan";
+import res from "express/lib/response";
 
 const PORT = 4000;
 
-//express() function을 사용하면 express application을 생성해줌
 const app = express();
+
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  console.log("I will respond");
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-// 어떤 port를 listening할 건지
 app.listen(PORT, handleListening);
