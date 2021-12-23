6693a68dffc2f99f8f1d24bc947e2952c4eb9ee5
xoy8n <s01057111917@gmail.com>
2021-12-23 14:58:42 +0900

    #4.3 Using Parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,15 @@
 export const trending = (req, res) => res.send("Home page videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.sent("Edit");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send(`Watch Video #${req.params.id}`);
+}; //{id : '121212'}
+// express는 내가 고른 이름과 함께 값을 제공해줌
+export const edit = (req, res) => {
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Video");
+};
