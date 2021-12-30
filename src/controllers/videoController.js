5f00147a56f3845692d28a554bba6dea25b86b89
xoy8n <s01057111917@gmail.com>
2021-12-30 15:32:54 +0900

    #5.1 configuring PUG

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,36 @@
-export const trending = (req, res) => res.send("Home page videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
-}; //{id : '121212'}
-// express는 내가 고른 이름과 함께 값을 제공해줌
-export const edit = (req, res) => {
-  return res.send("Edit");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAT: "2 minutes ago",
+      view: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAT: "2 minutes ago",
+      view: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAT: "2 minutes ago",
+      view: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   return res.send("Delete Video");
 };
