e0ffa9f7e356ee384d8f335703a186b3be8ec1b4
xoy8n <s01057111917@gmail.com>
2022-01-08 10:17:23 +0900

    #6.5 search part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 //Video.find({}, (error, videos) => {});
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "asc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -60,4 +60,17 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   //delete video
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
