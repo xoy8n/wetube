e2fe5d5e37f3f79f95ff5f369cf96c6ff4be2cc1
xoy8n <s01057111917@gmail.com>
2022-01-18 11:22:38 +0900

    #8.4 video Upload

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,6 +3,7 @@ import Mongoose from "mongoose";
 const videoSchema = new Mongoose.Schema({
   // 이 코드 안에 비디오 형식을 작성
   title: { type: String, required: true, trim: true, maxlength: 80 },
+  fileUrl: { type: String, required: true },
   description: { type: String, required: true, trim: true, minlength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
@@ -13,7 +14,9 @@ const videoSchema = new Mongoose.Schema({
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-  return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = Mongoose.model("Video", videoSchema);
