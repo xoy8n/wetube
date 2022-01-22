434bb185496fafcc329ce36d59c1ffb952acea84
xoy8n <s01057111917@gmail.com>
2022-01-22 16:12:23 +0900

    #8.8 User's video

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -225,16 +225,15 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res
       .status(404)
       .render("404", { pageTitle: "사용자를 찾을 수 없습니다." });
   }
-  const videos = await Video.find({ owner: user._id });
+
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
-    videos,
   });
 };
