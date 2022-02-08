456497b54a578bcd95c6ea7808b0f8db578e0593
xoy8n <s01057111917@gmail.com>
2022-02-08 12:24:22 +0900

    #10.2 video.scss

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,9 @@ import User from "../models/User.js";
 
 //Video.find({}, (error, videos) => {});
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "asc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
