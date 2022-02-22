0d1b9126fc6ad271a547860580f98304ef4a623f
xoy8n <s01057111917@gmail.com>
2022-02-22 14:25:13 +0900

    #13.2 Downloading Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,29 +2,52 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+let videoFile;
+
+// const handleDownload = () => {
+//   const a = document.createElement("a");
+//   a.href = videoFile;
+//   a.download = "MyRecording.webm";
+//   document.body.appendChild(a);
+//   a.click();
+// };
+
+const handleDownload = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleDownload);
+  startBtn.addEventListener("click", handleStart);
+
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "My Recording.mp4";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  init();
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = (e) => {
-    console.log("recording done");
-    console.log(e);
-    console.log(e.data);
+  recorder = new window.MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
-  console.log(recorder);
   recorder.start();
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
