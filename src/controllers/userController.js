e0d4bd590fc1582163f5114be0a2e0a25aa8521c
xoy8n <s01057111917@gmail.com>
2022-04-01 13:35:52 +0900

    #17.5 s3 uploads

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -177,7 +177,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       email,
       name,
       username,
