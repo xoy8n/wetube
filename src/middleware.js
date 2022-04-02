db0027d4960263a827737bf4bfafe341d515cd35
xoy8n <s01057111917@gmail.com>
2022-04-02 18:41:27 +0900

    #17.5 s3 uploads2

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,11 @@ const s3 = new aws.S3({
 const multerUploader = multerS3({
   s3: s3,
   bucket: "xoy8n-tube",
-  acl: "public-read",
+  Condition: {
+    StringEquals: {
+      "s3:x-amz-acl": ["public-read"],
+    },
+  },
 });
 
 export const localsMiddleware = (req, res, next) => {
